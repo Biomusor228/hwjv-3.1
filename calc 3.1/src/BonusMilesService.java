11d1723c0a512f623aefee7698e0ff4157fca656
Arsen <fiodoroff.mir@yandex.ru>
2025-08-22 18:15:34 +0300

    Fix: complete program with all lines

--- calc 3.1/src/BonusMilesService.java
+++ calc 3.1/src/BonusMilesService.java
@@ -1,6 +1,5 @@
-// Это - класс, вызываемый алгоритм приложения
 public class BonusMilesService {
-    public int calculate (int ticketPrice){
+    public int calculate(int ticketPrice){
         int rublesPerMile = 20;
         return ticketPrice / rublesPerMile;
     }
--- calc 3.1/src/Main.java
+++ calc 3.1/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        BonusMilesService service /*(Это - сервис, приложение)*/ = new BonusMilesService(); // (Это - объект, готовая формула, работающая по вызываемому алгоритму приложения)
+        BonusMilesService service = new BonusMilesService();
 
-        int ticketPrice1 = 10_000; //Стоимость билета
-        int bonusMiles1 = service.calculate(ticketPrice1); // Бонусные мили равны вытаскиваемому классу (ячейке), высчитывающему количество милей (должно получиться 500)
-        System.out.println("За билет стоимостью " + ticketPrice1 + " рублей начислено " + bonusMiles1 + " бонусных миль."); // Вывод бонусных милей
+        int ticketPrice1 = 10_000;
+        int bonusMiles1 = service.calculate(ticketPrice1);
+        System.out.println("За билет стоимостью " + ticketPrice1 + " рублей начислено " + bonusMiles1 + " бонусных миль.");
 
         int ticketPrice2 = 13_676;
         int bonusMiles2 = service.calculate(ticketPrice2);
